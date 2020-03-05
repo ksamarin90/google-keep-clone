ff825bbff62dd61baf71b4880a0cff580d2ea099
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2020-03-05 18:03:27 +0300

    Addition of Notes
    Notes area is built. Notes can be added, if title or text are provided. After clicking submit or outside of form area entries will be deleted and form closed.  Notes can't be deleted yet. Toolbar in every note is provided (hover effect).

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 class App {
   constructor() {
+    this.notes = [];
+
     this.$form = document.querySelector('#form');
     this.$noteTitle = document.querySelector('#note-title');
+    this.$noteText = document.querySelector('#note-text');
     this.$formButtons = document.querySelector('#form-buttons');
+    this.$placeholder = document.querySelector('#placeholder');
+    this.$notes = document.querySelector('#notes');
 
     this.addEventListeners();
   }
@@ -10,6 +15,16 @@ class App {
   addEventListeners() {
     document.body.addEventListener('click', event => {
       this.handleFormClick(event);
+    });
+
+    this.$form.addEventListener('submit', event => {
+      event.preventDefault();
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
+      const hasNote = title || text;
+      if (hasNote) {
+        this.addNote({ title, text });
+      }
     });
   }
 
@@ -33,6 +48,40 @@ class App {
     this.$form.classList.remove('form-open');
     this.$noteTitle.style.display = 'none';
     this.$formButtons.style.display = 'none';
+    this.$noteTitle.value = '';
+    this.$noteText.value = '';
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: 'white',
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+    };
+    this.notes = [...this.notes, newNote];
+    this.displayNotes();
+  }
+
+  displayNotes() {
+    const hasNotes = this.notes.length > 0;
+    this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
+    
+    this.$notes.innerHTML =
+    this.notes.map(note => `
+      <div style="background: ${note.color};" class="note">
+        <div class="${note.title && 'note-title'}">${note.title}</div>
+        <div class="note-text">${note.text}</div>
+        <div class="toolbar-container">
+          <div class="toolbar">
+            <img class="toolbar-color" src="https://icon.now.sh/palette">
+            <img class="toolbar-delete" src="https://icon.now.sh/delete">
+          </div>
+        </div>
+      </div>
+    `).join('');
+
+    this.closeForm();
   }
 
 };
